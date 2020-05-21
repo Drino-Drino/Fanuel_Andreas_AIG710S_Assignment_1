539a38eb327bbc630f024993441afbf2369c6ea4
Drino-Drino <kondjakudumo@yahoo.com>
2020-05-22 01:20:34 +0200

    Keep it safe

--- Bank_Research.jl
+++ Bank_Research.jl
@@ -45,18 +45,41 @@ Data[Data[:job].=="unknown",:job] = StatsBase.mode(Data[:job])
 Pkg.add("CategoricalArrays")
 using CategoricalArrays
 MaritalVector = CategoricalArray(Data[:marital]) #A vector of the marital column
-levels(BVector) #Checking the different Categories in the marital column, incase there are missing values
+levels(MaritalVector) #Checking the different Categories in the marital column, incase there are missing values
 
 #We replace unknown by the mode of the marital column
 StatsBase.mode(Data[:marital]) #checking the mode
 Data[Data[:marital].=="unknown",:marital] = StatsBase.mode(Data[:marital])
 
 #We repeat the above process for all categorical data columns
+EducationVector = CategoricalArray(Data[:education])
+levels(EducationVector) #checking the categories, we notice "unknown"
+StatsBase.mode(Data[:education]) #checking the mode != unknown
+Data[Data[:education].=="unknown",:education] = StatsBase.mode(Data[:education])
 
+#default column cleaning
+DefaultVector = CategoricalArray(Data[:default]) #we notice "unknown" values straight away
+levels(DefaultVector) #check for unknown values
+StatsBase.mode(Data[:default]) #we check the mode and it is = no
+Data[Data[:default].=="unknown",:default] = StatsBase.mode(Data[:default])
+
+#housing column cleaning
+HousingVector = CategoricalArray(Data[:housing])
+levels(HousingVector)
+StatsBase.mode(Data[:housing])
+Data[Data[:housing].=="unknown",:housing] = StatsBase.mode(Data[:housing])
+
+#Cleaning loan column
+LoanVector = CategoricalArray(Data[:loan])
+levels(LoanVector)
+StatsBase.mode(Data[:loan])
+Data[Data[:loan].=="unknown",:loan] = StatsBase.mode(Data[:loan])
+
+#=
 
 #train[isna.(train[:Married]), :Married] = mode(dropna(train[:Married])) 
 #replace 0.0 of loan amount with the mean of loan amount 
-#train[train[:LoanAmount] .== 0, :LoanAmount] = floor(mean(dropna(train[:LoanAmount])))
+#train[train[:LoanAmount] .== 0, :LoanAmount] = floor(mean(dropna(train[:LoanAmount])))=#
 
 
 #DataMatrix = convert(Matrix, Data)
